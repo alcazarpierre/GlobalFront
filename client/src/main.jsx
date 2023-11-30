fb70aca8241da1b85ba0c8fee0eb70a20f075170
alcazarpierre <alcazarpierre@gmail.com>
2023-11-30 11:06:04 -0500

    HashRouter

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import store from './redux/store.js';
 import { Provider } from 'react-redux';
 import axios from 'axios';
@@ -24,10 +25,10 @@ axios.interceptors.request.use(
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 <Provider store = {store}>
-  <BrowserRouter>
+  <Router>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </BrowserRouter>
+  </Router>
 </Provider>
 )
